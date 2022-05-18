60feac77786e6ad0868ee53857b03025323cdf37
dattali18 <danielattali16@gmail.com>
2022-05-18 16:30:51 +0300

    commit 2

---
+++ Queue.py
@@ -0,0 +1,17 @@
+class Queue:
+    data: list
+
+    def __init__(self):
+        self.data = []
+
+    def pop(self):
+        return self.data.pop()
+
+    def push(self, value):
+        self.data.insert(value)
+
+    def enqueue(self):
+        return self.pop()
+
+    def dequeue(self, value):
+        self.push(value)
